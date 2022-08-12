cda799404a5bac20d0c933bc57938299624608be
Alex Wohlhueter <wallheater@gmail.com>
2022-08-12 14:05:44 -0500

    Render 1 file per day
    With pagination nav, sticky to top

--- index.js
+++ index.js
@@ -9,22 +9,21 @@ const mustache = require('mustache');
 const outfile = 'schedule.html';
 const baseDomain = 'https://minnesotafringe.org';
 const scheduleUrl = 'https://minnesotafringe.org/2022/schedule?d=19978';
-// https://minnesotafringe.org/2022/schedule?d=19988
 
 // Fringe site has an unknown dayNumber concept:
 const dayNumOffset = 19977;
 const festDays = [
-    {dayNum:  1, label: 'Thu 08/04', dateStr: '08/04'},
-    {dayNum:  2, label: 'Fri 08/05', dateStr: '08/05'},
-    {dayNum:  3, label: 'Sat 08/06', dateStr: '08/06'},
-    {dayNum:  4, label: 'Sun 08/07', dateStr: '08/07'},
-    {dayNum:  5, label: 'Mon 08/08', dateStr: '08/08'},
-    {dayNum:  6, label: 'Tue 08/09', dateStr: '08/09'},
-    {dayNum:  7, label: 'Wed 08/10', dateStr: '08/10'},
-    {dayNum:  8, label: 'Thu 08/11', dateStr: '08/11'},
-    {dayNum:  9, label: 'Fri 08/12', dateStr: '08/12'},
-    {dayNum: 10, label: 'Sat 08/13', dateStr: '08/13'},
-    {dayNum: 11, label: 'Sun 08/14', dateStr: '08/14'},
+    {dayNum:  1, dateStr: '08/04', slug: '804', dow: 'Thu', dom:  '4', label: 'Thursday 8/04'},
+    {dayNum:  2, dateStr: '08/05', slug: '805', dow: 'Fri', dom:  '5', label: 'Friday 8/05'},
+    {dayNum:  3, dateStr: '08/06', slug: '806', dow: 'Sat', dom:  '6', label: 'Saturday 8/06'},
+    {dayNum:  4, dateStr: '08/07', slug: '807', dow: 'Sun', dom:  '7', label: 'Sunday 8/07'},
+    {dayNum:  5, dateStr: '08/08', slug: '808', dow: 'Mon', dom:  '8', label: 'Monday 8/08'},
+    {dayNum:  6, dateStr: '08/09', slug: '809', dow: 'Tue', dom:  '9', label: 'Tuesday 8/09'},
+    {dayNum:  7, dateStr: '08/10', slug: '810', dow: 'Wed', dom: '10', label: 'Wednesday 8/10'},
+    {dayNum:  8, dateStr: '08/11', slug: '811', dow: 'Thu', dom: '11', label: 'Thursday 8/11'},
+    {dayNum:  9, dateStr: '08/12', slug: '812', dow: 'Fri', dom: '12', label: 'Friday 8/12'},
+    {dayNum: 10, dateStr: '08/13', slug: '813', dow: 'Sat', dom: '13', label: 'Saturday 8/13'},
+    {dayNum: 11, dateStr: '08/14', slug: '814', dow: 'Sun', dom: '14', label: 'Sunday 8/14'},
 ];
 
 function scrapeTest() {
@@ -65,43 +64,54 @@ function build() {
     console.log("build()");
     const collectorRaw = fs.readFileSync('schedule.json');
     let collector = JSON.parse(collectorRaw);
-    finalRender(collector);
+    // finalRender(collector);
+    festDays.forEach( function(day) {
+        // console.log('festDay', day);
+        renderPage(collector, day.dayNum);
+    });
+    // renderPage(collector, 9);
+
 }
 
-function finalRender(collector) {
-    // console.log('collector', collector);
+function renderPage(collector, dayNum) {
+    console.log('dayNum', dayNum);
     // @todo sort?
-    let dayData = Object.values(collector.days).map( function(day) {
-        day.dayLabel = festDays.find(v => v.dayNum == day.dayNum).label;
+    // let dayData = Object.values(collector.days).map( function(day) {
+    //     day.dayLabel = festDays.find(v => v.dayNum == day.dayNum).label;
+    //     return day;
+    // });
+
+    const dayEvents = Object.values(collector.days).find( function(day) {
+        return !!day && day.dayNum == dayNum;
+    }).events;
+
+    const dayNav = festDays.map( function (dayRef) {
+        let day = {...dayRef};
+        day['active'] = (day.dayNum == dayNum) ? 'active' : '';
         return day;
     });
+    // console.log('dayNav', dayNav);
+    const dayMeta = festDays.find( function (day) {
+        return day.dayNum == dayNum;
+    });
 
-    data = {
+    const data = {
         docTitle: '2022 Schedule',
         buildTime: new Date(collector.scrapeTime).toLocaleString(),
-        dayNav: festDays,
-        days: dayData,
+        dayNav: dayNav,
+        dayLabel: dayNum,
+        dayMeta: dayMeta,
+        // days: dayData,
+        events: dayEvents,
     };
     fs.readFile('schedule.mustache', function (err, template) {
         if (err) throw err;
         const content = mustache.render(template.toString(), data);
-        fs.writeFile(outfile, content, err => {
+        fs.writeFile('schedule-' + dayMeta.slug + '.html', content, err => {
             if (err) throw err;
             // file written successfully
         });
     });
-}
-
-function renderRows(collector) {
-    let content = '';
-    const keys = Object.keys(collector).sort();
-    keys.forEach( function (k) {
-        console.log('rendering', k);
-        collector[k].forEach( function(event) {
-            content += renderRow(event);
-        });
-    });
-    return content;
 }
 
 function testParse() {
@@ -138,8 +148,6 @@ async function readLocalFile(path, i, collector) {
     collector.days[i] = {
         events: events,
         dayNum: i,
-        prevDay: (i == 1) ? undefined : [{dayNum: i - 1}],
-        nextDay: (i == 11) ? undefined : [{dayNum: i + 1}],
     };
 }
 
@@ -152,8 +160,6 @@ async function readUrl(url, i, collector) {
         collector.days[i] = {
             events: events,
             dayNum: i,
-            prevDay: (i == 1) ? undefined : [{dayNum: i - 1}],
-            nextDay: (i == 11) ? undefined : [{dayNum: i + 1}],
         };
     } catch (error) {
         console.error(error);
@@ -186,5 +192,5 @@ function parseSchedule(content) {
 
 // main().catch(console.error);
 // buildTest();
-scrape();
-// build();
+// scrape();
+build();
