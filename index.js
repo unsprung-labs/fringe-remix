8ff4311321542c9bfe44faa28b39783a6bf7d58a
Alex Wohlhueter <wallheater@gmail.com>
2023-07-10 09:17:38 -0500

    Update for 2023 dates and CMS changes
    New days & dates.
    Schedule page day param is now ISO date, not dayNum.
    Some text values needed trim() due to new whitespace.
    Moved updatedAt to footer.

--- index.js
+++ index.js
@@ -8,22 +8,24 @@ const mustache = require('mustache');
 
 const outfile = 'schedule.html';
 const baseDomain = 'https://minnesotafringe.org';
+const festYear = '2023';
 const scheduleUrl = 'https://minnesotafringe.org/2022/schedule?d=19978';
+// New fmt: https://minnesotafringe.org/schedule/2023?d=2023-08-03#schedule
 
-// Fringe site has an unknown dayNumber concept:
+// Fringe site had an unknown dayNumber concept:
 const dayNumOffset = 19977;
 const festDays = [
-    {dayNum:  1, dateStr: '08/04', slug: '804', dow: 'Thu', dom:  '4', label: 'Thursday 8/04'},
-    {dayNum:  2, dateStr: '08/05', slug: '805', dow: 'Fri', dom:  '5', label: 'Friday 8/05'},
-    {dayNum:  3, dateStr: '08/06', slug: '806', dow: 'Sat', dom:  '6', label: 'Saturday 8/06'},
-    {dayNum:  4, dateStr: '08/07', slug: '807', dow: 'Sun', dom:  '7', label: 'Sunday 8/07'},
-    {dayNum:  5, dateStr: '08/08', slug: '808', dow: 'Mon', dom:  '8', label: 'Monday 8/08'},
-    {dayNum:  6, dateStr: '08/09', slug: '809', dow: 'Tue', dom:  '9', label: 'Tuesday 8/09'},
-    {dayNum:  7, dateStr: '08/10', slug: '810', dow: 'Wed', dom: '10', label: 'Wednesday 8/10'},
-    {dayNum:  8, dateStr: '08/11', slug: '811', dow: 'Thu', dom: '11', label: 'Thursday 8/11'},
-    {dayNum:  9, dateStr: '08/12', slug: '812', dow: 'Fri', dom: '12', label: 'Friday 8/12'},
-    {dayNum: 10, dateStr: '08/13', slug: '813', dow: 'Sat', dom: '13', label: 'Saturday 8/13'},
-    {dayNum: 11, dateStr: '08/14', slug: '814', dow: 'Sun', dom: '14', label: 'Sunday 8/14'},
+    {dayNum:  1, isoDate: '2023-08-03', dateStr: '08/03', slug: '803', dow: 'Thu', dom:  '3', label: 'Thursday 8/03'},
+    {dayNum:  2, isoDate: '2023-08-04', dateStr: '08/04', slug: '804', dow: 'Fri', dom:  '4', label: 'Friday 8/04'},
+    {dayNum:  3, isoDate: '2023-08-05', dateStr: '08/05', slug: '805', dow: 'Sat', dom:  '5', label: 'Saturday 8/05'},
+    {dayNum:  4, isoDate: '2023-08-06', dateStr: '08/06', slug: '806', dow: 'Sun', dom:  '6', label: 'Sunday 8/06'},
+    {dayNum:  5, isoDate: '2023-08-07', dateStr: '08/07', slug: '807', dow: 'Mon', dom:  '7', label: 'Monday 8/07'},
+    {dayNum:  6, isoDate: '2023-08-08', dateStr: '08/08', slug: '808', dow: 'Tue', dom:  '8', label: 'Tuesday 8/08'},
+    {dayNum:  7, isoDate: '2023-08-09', dateStr: '08/09', slug: '809', dow: 'Wed', dom:  '9', label: 'Wednesday 8/09'},
+    {dayNum:  8, isoDate: '2023-08-10', dateStr: '08/10', slug: '810', dow: 'Thu', dom: '10', label: 'Thursday 8/10'},
+    {dayNum:  9, isoDate: '2023-08-11', dateStr: '08/11', slug: '811', dow: 'Fri', dom: '11', label: 'Friday 8/11'},
+    {dayNum: 10, isoDate: '2023-08-12', dateStr: '08/12', slug: '812', dow: 'Sat', dom: '12', label: 'Saturday 8/12'},
+    {dayNum: 11, isoDate: '2023-08-13', dateStr: '08/13', slug: '813', dow: 'Sun', dom: '13', label: 'Sunday 8/13'},
 ];
 
 function scrapeTest() {
@@ -50,11 +52,11 @@ function scrape() {
         days: [],
     };
     let parsePromises = festDays.map( function(d) {
-        let fringeDay = d.dayNum + dayNumOffset;
-        return readUrl(`https://minnesotafringe.org/2022/schedule?d=${fringeDay}`, d.dayNum, collector);
+        // let fringeDay = d.dayNum + dayNumOffset;
+        return readUrl(`https://minnesotafringe.org/schedule/${festYear}?d=${d.isoDate}`, d.dayNum, collector);
     });
     Promise.all(parsePromises).then( (values) => {
-        console.log('all done!');
+        console.log('all done! writing schedule.json...');
         fs.writeFileSync('schedule.json', JSON.stringify(collector));
        // finalRender(collector);
     });
@@ -83,7 +85,10 @@ function renderPage(collector, dayNum) {
 
     const dayEvents = Object.values(collector.days).find( function(day) {
         return !!day && day.dayNum == dayNum;
-    }).events;
+    }).events.map( function(event) {
+        event['tim'] = event.time.replace(' PM', '');
+        return event;
+    });
 
     const dayNav = festDays.map( function (dayRef) {
         let day = {...dayRef};
@@ -96,7 +101,7 @@ function renderPage(collector, dayNum) {
     });
 
     const data = {
-        docTitle: '2022 Schedule',
+        docTitle: `${festYear} Schedule`,
         buildTime: new Date(collector.scrapeTime).toLocaleString(),
         dayNav: dayNav,
         dayLabel: dayNum,
@@ -174,12 +179,12 @@ function parseSchedule(content) {
     var events = [];
     $(inRows).each( function (i, e) {
         let event = {};
-        event.time = $(this).find('td.tdevent').text();
-        event.venue = $(this).find('td.tdvenue a').text();
+        event.time = $(this).find('td.tdevent').text().trim();
+        event.venue = $(this).find('td.tdvenue a').text().trim();
         event.showTitle = $(this).find('td.tdshow a').text();
         event.showUrl = baseDomain + $(this).find('td.tdshow a').attr('href');
         // parse tags like "AD" from day
-        let dayTag = $(this).find('td.tddate').text();
+        let dayTag = $(this).find('td.tddate').text().trim();
         const dtrx = /^(\d+\/\d+)\s*(.*)/;
         event.date = dayTag.match(dtrx)[1];
         event.serviceTags = dayTag.match(dtrx)[2];
