4a94076f4bb577d0669b9020cf14a4e853737d1a
Alex Wohlhueter <wallheater@gmail.com>
2024-07-01 22:16:24 -0400

    Update consts for 2024

--- remix.js
+++ remix.js
@@ -6,19 +6,21 @@ const mustache = require('mustache');
 // SETUP
 
 const baseDomain = 'https://minnesotafringe.org';
-const festYear = '2023';
+const festYear = '2024';
+const festLengthDays = 11;
+const festStart = '2024-08-01';
 const festDays = [
-    {dayNum:  1, isoDate: '2023-08-03', dateStr: '08/03', slug: '803', dow: 'Thu', dom:  '3', label: 'Thursday 8/03'},
-    {dayNum:  2, isoDate: '2023-08-04', dateStr: '08/04', slug: '804', dow: 'Fri', dom:  '4', label: 'Friday 8/04'},
-    {dayNum:  3, isoDate: '2023-08-05', dateStr: '08/05', slug: '805', dow: 'Sat', dom:  '5', label: 'Saturday 8/05'},
-    {dayNum:  4, isoDate: '2023-08-06', dateStr: '08/06', slug: '806', dow: 'Sun', dom:  '6', label: 'Sunday 8/06'},
-    {dayNum:  5, isoDate: '2023-08-07', dateStr: '08/07', slug: '807', dow: 'Mon', dom:  '7', label: 'Monday 8/07'},
-    {dayNum:  6, isoDate: '2023-08-08', dateStr: '08/08', slug: '808', dow: 'Tue', dom:  '8', label: 'Tuesday 8/08'},
-    {dayNum:  7, isoDate: '2023-08-09', dateStr: '08/09', slug: '809', dow: 'Wed', dom:  '9', label: 'Wednesday 8/09'},
-    {dayNum:  8, isoDate: '2023-08-10', dateStr: '08/10', slug: '810', dow: 'Thu', dom: '10', label: 'Thursday 8/10'},
-    {dayNum:  9, isoDate: '2023-08-11', dateStr: '08/11', slug: '811', dow: 'Fri', dom: '11', label: 'Friday 8/11'},
-    {dayNum: 10, isoDate: '2023-08-12', dateStr: '08/12', slug: '812', dow: 'Sat', dom: '12', label: 'Saturday 8/12'},
-    {dayNum: 11, isoDate: '2023-08-13', dateStr: '08/13', slug: '813', dow: 'Sun', dom: '13', label: 'Sunday 8/13'},
+    {dayNum:  1, isoDate: '2024-08-01', dateStr: '08/01', slug: '801', dow: 'Thu', dom:  '1', label: 'Thursday 8/01'},
+    {dayNum:  2, isoDate: '2024-08-02', dateStr: '08/02', slug: '802', dow: 'Fri', dom:  '2', label: 'Friday 8/02'},
+    {dayNum:  3, isoDate: '2024-08-03', dateStr: '08/03', slug: '803', dow: 'Sat', dom:  '3', label: 'Saturday 8/03'},
+    {dayNum:  4, isoDate: '2024-08-04', dateStr: '08/04', slug: '804', dow: 'Sun', dom:  '4', label: 'Sunday 8/04'},
+    {dayNum:  5, isoDate: '2024-08-05', dateStr: '08/05', slug: '805', dow: 'Mon', dom:  '5', label: 'Monday 8/05'},
+    {dayNum:  6, isoDate: '2024-08-06', dateStr: '08/06', slug: '806', dow: 'Tue', dom:  '6', label: 'Tuesday 8/06'},
+    {dayNum:  7, isoDate: '2024-08-07', dateStr: '08/07', slug: '807', dow: 'Wed', dom:  '7', label: 'Wednesday 8/07'},
+    {dayNum:  8, isoDate: '2024-08-08', dateStr: '08/08', slug: '808', dow: 'Thu', dom: '8', label: 'Thursday 8/08'},
+    {dayNum:  9, isoDate: '2024-08-09', dateStr: '08/09', slug: '809', dow: 'Fri', dom: '9', label: 'Friday 8/09'},
+    {dayNum: 10, isoDate: '2024-08-10', dateStr: '08/10', slug: '810', dow: 'Sat', dom: '10', label: 'Saturday 8/10'},
+    {dayNum: 11, isoDate: '2024-08-11', dateStr: '08/11', slug: '811', dow: 'Sun', dom: '11', label: 'Sunday 8/11'},
 ];
 const venues = [
     {venue: "Augsburg Mainstage"},
